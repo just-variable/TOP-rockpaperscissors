3ad0ca3b3f870e80d544f6cd886a8f9767c6b4f3
simon <simonhaidar2@icloud.com>
2024-04-01 18:44:42 +0300

    add ui

--- index.js
+++ index.js
@@ -11,22 +11,43 @@ function playRound(userChoice, computerChoice){
     }
 
     if(userChoice == computerChoice){
-        return `It's a tie!`;
+        return 0;
     }
 
     if(rules[userChoice] == computerChoice){
-        return `You win, ${userChoice} beats ${computerChoice}`;
+        return 1;
     } else {
-        return `You lose, ${computerChoice} beats ${userChoice}`;
+        return -1;
     }
 
 }
 
 function playGame(){
-    for(let i=0; i<5; i++){
+    // for(let i=0; i<5; i++){
         let userChoice = prompt("Rock/Paper/Scissors?");
         console.log(playRound(userChoice.toLowerCase(), getComputerChoice()));
-    }
+    // }
     
 }
-playGame();
+// playGame();
+
+let RPSbuttons = document.querySelectorAll("body button")
+
+let userScore = document.querySelector(".user-score");
+let computerScore = document.querySelector(".computer-score");
+
+RPSbuttons.forEach(b => {
+    b.addEventListener("click", (e) => {
+        switch (playRound(e.target.textContent.toLowerCase(), getComputerChoice())){
+            case 1:
+                userScore.textContent++;
+                break;
+            case -1:
+                computerScore.textContent++;
+                break;
+            case 0:
+                console.log("tie");
+                break;
+        }
+    })
+})
